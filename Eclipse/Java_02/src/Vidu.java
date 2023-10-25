5d41c78d8c1df931c4aa1bfb2370d85adb3bd55d
NguyenTrungThanh <22730019@ms.uit.edu.vn>
2023-10-26 05:27:19 +0700

    Buoi_1_2

---
+++ Eclipse/Java_02/src/Vidu.java
@@ -0,0 +1,14 @@
+
+public class Vidu {
+	public static void main(String[] args) {
+		String thongBao = "Xin Chao";
+		String hoVaTen = "Nguyen Trung Thanh"; 
+		String tenSach;
+		tenSach = "Lap Trinh Java";
+		
+		System.out.println("Thong Bao :"+ thongBao);
+		System.out.println("Ho Va Ten :" + hoVaTen);
+		System.out.println("Ten Sach: " + tenSach);
+	}
+
+}
