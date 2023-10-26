7ec3550e8550b6587fc2398914b92b497609c68e
Nguyen Trung Thanh <22730019@ms.uit.edu.vn>
2023-10-27 00:48:32 +0700

    Kieu du lieu

---
+++ Eclipse/Java_05/src/Vidu.java
@@ -0,0 +1,15 @@
+
+public class Vidu {
+	public static void main(String[] args) {
+		String tenSach = "Lap Trinh Java";
+		int namXuatBan = 2021;
+		double giaTien = 15.5;
+		boolean conSach = true;
+		char maKho = 'M';
+		System.out.println("Ten Sach: " + tenSach);
+		System.out.println("nam: " + namXuatBan);
+		System.out.println("Gia Tien : " + giaTien);
+		System.out.println("Con Sach : " + conSach);
+	}
+
+}
