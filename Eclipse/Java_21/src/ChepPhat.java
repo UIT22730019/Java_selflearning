0d1fb2d0587b99daf4685ac9849628c62d31e4d3
Nguyen Trung Thanh <22730019@ms.uit.edu.vn>
2023-10-27 06:15:21 +0700

    bangcuuchuong

---
+++ Eclipse/Java_21/src/ChepPhat.java
@@ -0,0 +1,10 @@
+
+public class ChepPhat {
+	public static void main(String[] args) {
+		//Bị phạt chép 20 lần "Em hứa sẽ làm bài tập đầy đủ"
+	for (int i = 0; i <= 2000; i++) {
+		System.out.println(i +"em hu se lam bai tap day du");
+	}
+	}
+
+}
---
+++ Eclipse/Java_22/src/InBangCuuChuong.java
@@ -0,0 +1,12 @@
+
+public class InBangCuuChuong {
+public static void main(String[] args) {
+	for (int i = 1; i <=9; i++) {
+		for (int j = 1; j <=10; j++) {
+			System.out.println(i + "x" + j + "=" + (i*j));
+		}
+		System.out.println("------------------");
+		
+	}
+}
+}
