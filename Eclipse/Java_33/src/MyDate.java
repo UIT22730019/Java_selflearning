a515719fc4bae2b7cecfa43113c55daebd4b0ecc
Nguyen Trung Thanh <22730019@ms.uit.edu.vn>
2023-10-31 09:32:07 +0700

    toString learning

---
+++ Eclipse/Java_33/src/MyDate.java
@@ -0,0 +1,57 @@
+
+public class MyDate {
+	private int day, month, year;
+
+	public MyDate(int day, int month, int year) {
+		if (day >= 1 && day <= 31) {
+			this.day = day;
+		} else {
+			this.day = 1;
+		}
+		if (month >= 1 && month <= 12) {
+			this.month = month;
+		} else {
+			this.month = month;
+		}
+		if (year >= 1) {
+			this.year = year;
+		} else {
+			this.year = 1;
+		}
+
+	}
+
+	public int getDay() {
+		return this.day;
+	}
+
+	public void setDay(int d) {
+		if (d >= 1 && d <= 31) {
+			this.day = d;
+		}
+	}
+
+	public int getMonth() {
+		return this.month;
+	}
+
+	public void setMonth(int m) {
+		if (m >= 1 && m <= 12)
+			this.month = m;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+
+	public void setYear(int y) {
+		if (year >= 1)
+			this.year = y;
+	}
+
+	@Override
+	public String toString() {
+		return this.day + "/" + this.month + "/" + this.year;
+	}
+
+}
---
+++ Eclipse/Java_33/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+	public static void main(String[] args) {
+		MyDate md1 = new MyDate(31, 2, 2022);
+		MyDate md2 = new MyDate(11, 1, 2062);
+		MyDate md3 = new MyDate(25, 7, 2011);
+		System.out.println(md1);
+		System.out.println(md2);
+		System.out.println(md3);
+		
+	}
+
+	
+	
+	
+}
